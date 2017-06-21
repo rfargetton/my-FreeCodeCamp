183cf90b09474b1b2d8134e2af311a20fdd9b949
rfargetton <romaric.fargetton@gmail.com>
2017-06-21 16:00:49 +0200

    initial commit

--- intermediate_front_end/wikipedia_viewer/main.js
+++ intermediate_front_end/wikipedia_viewer/main.js
@@ -1,15 +1,1 @@
 console.log('Hello Wikipedia Viewer');
-
-bullshit.doWhatever('bull', function(shit){
-  console.log();
-});
-
-for (var variable in object) {
-  if (object.hasOwnProperty(variable)) {
-
-  }
-}
-
-for (var i = 0; i < array.length; i++) {
-  array[i]
-}
